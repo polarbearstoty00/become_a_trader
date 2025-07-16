d503fe156d4f944721494235417071c1ec1bddba
polarbearstoty00 <polarbearstoty00@gmail.com>
2025-07-16 14:29:21 +0900

    Update page_01.py

--- pages/page_01.py
+++ pages/page_01.py
@@ -5,7 +5,7 @@ import json
 
 Upbit_Balance_API_URL = "https://port-0-mywts-investment-flask-m8u0vlaa031d4a0d.sel4.cloudtype.app/upbit_balances"
 
-resp = requests.get(Upbit_Balance_API_URL, timeout=10)
+resp = requests.get(Upbit_Balance_API_URL)
 data = resp.json()
 
 st.metric("KRW 잔고", f"{data['krw']:,.0f} 원")
