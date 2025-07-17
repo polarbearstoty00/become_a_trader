3235b1a801d7d7b7ed03270eddfc89393b35b2fb
polarbearstoty00 <polarbearstoty00@gmail.com>
2025-07-17 17:17:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ st.subheader("Become a trader")
 
 # 업비트 티커 정보 불러오기
 upbit_tickers = pyupbit.get_tickers()
+upbit_krw_tickers = [ticker for ticker in upbit_tickers if ticker.startswith("KRW-")]
 st.write("업비트")
-st.write(upbit_tickers)
+st.write(upbit_krw_tickers)
 
 # 빗썸 티커 정보 불러오기
 bithumb_tickers = Bithumb.get_tickers()
